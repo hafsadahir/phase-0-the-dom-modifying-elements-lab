9bcfca29ac867af91b2702396204d3da2d6684ee
hafsadahir <hafsa.dahir@student.moringaschool.com>
2022-08-12 23:02:07 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removeMain = document.getElementById('main');
+removeMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
